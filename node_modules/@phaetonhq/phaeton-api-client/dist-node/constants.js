b2dfd632e0d85d2d8fa67280b20a9b5ba59401c5
hotam-singh <hotam.hbti123@gmail.com>
2019-10-16 18:44:59 +0530

    modified commander package

---
+++ node_modules/@phaetonhq/phaeton-api-client/dist-node/constants.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GET = 'GET';
+exports.POST = 'POST';
+exports.PUT = 'PUT';
+exports.TESTNET_NETHASH = 'da3ed6a45429278bac2666961289ca17ad86595d33b31037615d4b8e8f158bba';
+exports.MAINNET_NETHASH = 'ed14889723f24ecc54871d058d98ce91ff2f973192075c0155ba2b7b70ad2511';
+exports.TESTNET_NODES = [
+    'https://testnet.phaeton.io:443',
+];
+exports.MAINNET_NODES = [
+    'https://node01.phaeton.io:443',
+    'https://node02.phaeton.io:443',
+    'https://node03.phaeton.io:443',
+    'https://node04.phaeton.io:443',
+    'https://node05.phaeton.io:443',
+    'https://node06.phaeton.io:443',
+    'https://node07.phaeton.io:443',
+    'https://node08.phaeton.io:443',
+];
+//# sourceMappingURL=constants.js.map
